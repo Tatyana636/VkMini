055a87c902fb1eac6ae68956fe58cc48d1d4f421
Tatyana <Asta3647@mail.ru>
2021-01-02 14:37:05 +0300

    Updates

---
+++ precache-manifest.a8ba3726e336d27034286d4412c1ee5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d503b6f604e171eda57dc9d0a45f23",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3149b3d2bdadafa6b0a5",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "6deaf9e9d2bd2d442a9d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3149b3d2bdadafa6b0a5",
+    "url": "./static/js/2.c408e657.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.c408e657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6deaf9e9d2bd2d442a9d",
+    "url": "./static/js/main.492d65d2.chunk.js"
+  },
+  {
+    "revision": "01323d27b58c456f89c0",
+    "url": "./static/js/runtime-main.3cb78fda.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
